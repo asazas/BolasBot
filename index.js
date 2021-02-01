5d955c2b303fc548c5511dd0ce3ed37e050ac478
asazas <asazas0@hotmail.com>
2021-02-01 11:58:12 +0100

    Arreglado bug de regex

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ client.on('message', message => {
 
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-	const args = message.content.slice(prefix.length).trim().split('/ +/');
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const commandName = args.shift().toLowerCase();
 
 	const command = client.commands.get(commandName)
